24a187afe7ea8f6bbeae980e1b205c2b83c2f52d
alexiakarine <alexiakarine18@gmail.com>
2023-05-03 19:20:51 -0300

    ok

--- src/main.js
+++ src/main.js
@@ -14,24 +14,7 @@ new Vue({
 }).$mount('#app')
 
 
-// import PrimeVue from 'primevue/config'
-// import { createApp } from 'vue'
 
-// import router from './router'
-// import store from './store'
-// import 'primevue/resources/themes/saga-blue/theme.css'     //theme
-// import 'primevue/resources/primevue.min.css'                 //core css
-// import 'primeicons/primeicons.css'                          //icons
-// import FileUpload from 'primevue/fileupload'
-// import ToastService from 'primevue/toastservice';
-
-// const app = createApp(App);
-// const app = createApp(App);
-// app.use(PrimeVue);
-// app.mount('#app');
-// app.use(router)
-// app.use(ToastService)
-// app.component('FileUpload', FileUpload);
 
 
 
